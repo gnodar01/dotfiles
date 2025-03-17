f84616050b9116034ec0ea3e35a27ce5c22bbd7d
Nodar Gogoberidze <gnodar01@gmail.com>
2025-03-17 11:05:16 -0400

    Setup up formatters

--- superhome/config/nvim/lua/config/mason-pkgs.lua
+++ superhome/config/nvim/lua/config/mason-pkgs.lua
@@ -3,7 +3,6 @@
 local mason_pkgs = {}
 
 -- Enable the following language servers.
--- Add/remove any desired LSPs.
 --
 -- `lsp_definitions` is a list of pairs.
 --
@@ -23,43 +22,43 @@ local mason_pkgs = {}
 --    - settings (table): Override the default settings passed when initializing the server.
 --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 local lsp_definitions = {
-  -- clangd = { install = 'clangd', config = {} },
-  -- gopls = { install = 'gopls', config = {} },
-  -- pyright = { install = 'pyright', config = {} },
-  -- rust_analyzer = { install = 'rust_analyzer', config = {} },
-  -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-  --
-  -- Some languages (like typescript) have entire language plugins that can be useful:
-  --    https://github.com/pmizio/typescript-tools.nvim
-  --
-  -- But for many setups, the LSP (`ts_ls`) will work just fine
-  -- ts_ls = { install = 'ts_ls', config = {} },
+	-- clangd = { install = 'clangd', config = {} },
+	-- gopls = { install = 'gopls', config = {} },
+	-- pyright = { install = 'pyright', config = {} },
+	-- rust_analyzer = { install = 'rust_analyzer', config = {} },
+	-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+	--
+	-- Some languages (like typescript) have entire language plugins that can be useful:
+	--    https://github.com/pmizio/typescript-tools.nvim
+	--
+	-- But for many setups, the LSP (`ts_ls`) will work just fine
+	-- ts_ls = { install = 'ts_ls', config = {} },
 
-  lua_ls = {
-    install = 'lua_ls',
-    config = {
-      -- cmd = { ... },
-      -- filetypes = { ... },
-      -- capabilities = {},
-      settings = {
-        Lua = {
-          completion = {
-            callSnippet = 'Replace',
-          },
-          -- toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-          -- diagnostics = { disable = { 'missing-fields' } },
-        },
-      },
-    }
-  },
+	lua_ls = {
+		install = "lua_ls",
+		config = {
+			-- cmd = { ... },
+			-- filetypes = { ... },
+			-- capabilities = {},
+			settings = {
+				Lua = {
+					completion = {
+						callSnippet = "Replace",
+					},
+					-- toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+					-- diagnostics = { disable = { 'missing-fields' } },
+				},
+			},
+		},
+	},
 }
 
 -- These go to `mason-lspconfig` in `plugins/lsp`
 mason_pkgs.lsp_server_configs = {}
 for name, data in pairs(lsp_definitions) do
-  if data.config then
-    mason_pkgs.lsp_server_configs[name] = data.config
-  end
+	if data.config then
+		mason_pkgs.lsp_server_configs[name] = data.config
+	end
 end
 
 -- Ensure the servers and tools above are installed
@@ -70,23 +69,48 @@ end
 --
 -- press `g?` for help in this menu.
 --
--- Add other tools here for Mason to install.
+-- Add other tools below for Mason to install.
 -- Registry:
 -- https://mason-registry.dev/registry/list
 
-mason_pkgs.formatters = {
-  -- https://github.com/JohnnyMorganz/StyLua
-  -- Lua language formatter
-  'stylua', -- used to format Lua code
+-- Enable the following formatters.
+--
+-- `formatter_definitions` is a table of `ext_name` to a list of definitions.
+--   The definintion is a list of strings of the formatter names.
+--
+--   A mapping of `ext_name` to the formatter names goes to `conform`,
+--     in the `formatters_by_ft` field
+--   A combined list of just the formatter names, goes to `mason-tool-installer`.
+--
+-- View configured and available formatters
+--   `:ConformInfo`
+local formatter_definitions = {
+	-- https://github.com/JohnnyMorganz/StyLua
+	-- Lua language formatter
+	lua = { "stylua" },
+
+	-- `conform` can also run multiple formatters sequentially
+	-- python = { "isort", "black" },
+	--
+	-- can use 'stop_after_first' to run the first available formatter from the list
+	-- javascript = { "prettierd", "prettier", stop_after_first = true },
 }
+
+-- These go to `conform` in the `formatters_by_ft` field.
+mason_pkgs.ext_formatters = formatter_definitions
+
+local formatters = {}
+for _, frmtrs in pairs(mason_pkgs.ext_formatters) do
+	vim.list_extend(formatters, frmtrs)
+end
 
 -- These go to `mason-tool-installer` in `plugins/mason`.
 mason_pkgs.ensure_installed = {}
 for _, data in pairs(lsp_definitions) do
-  if data.install then
-    table.insert(mason_pkgs.ensure_installed, data.install)
-  end
+	if data.install then
+		table.insert(mason_pkgs.ensure_installed, data.install)
+	end
 end
-vim.list_extend(mason_pkgs.ensure_installed, mason_pkgs.formatters)
+vim.list_extend(mason_pkgs.ensure_installed, formatters)
 
 return mason_pkgs
--- superhome/config/nvim/lua/plugins/misc.lua
+++ superhome/config/nvim/lua/plugins/misc.lua
@@ -3,6 +3,13 @@
 return {
   -- https://github.com/folke/todo-comments.nvim
   -- Highlight todo, notes, etc in comments
+  --
+  -- Use quickfix list to show todos
+  --   `:TodoQuickFix`
+  -- Use location list to show all todos
+  --   `:TodoLocList`
+  -- Use Telescope to search through todos
+  --   `:TodoTelescope`
   {
     'folke/todo-comments.nvim',
     name = 'todo-comments',
