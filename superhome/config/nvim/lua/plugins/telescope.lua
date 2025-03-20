fb46fc1605939cad7833e1ffa01faf2f4cd7ec88
Nodar Gogoberidze <gnodar01@gmail.com>
2025-03-20 16:05:13 -0400

    Add git files search in telescope

--- superhome/config/nvim/lua/plugins/telescope.lua
+++ superhome/config/nvim/lua/plugins/telescope.lua
@@ -62,6 +62,7 @@ return {
     -- See `:help telescope.builtin`
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<Leader>ff', builtin.find_files, { desc = '[F]uzzy Search [F]iles' })
+    vim.keymap.set('n', '<Leader>fF', builtin.git_files, { desc = '[F]uzzy Search Git [F]iles' })
     vim.keymap.set('n', '<Leader>fg', builtin.live_grep, { desc = '[F]uzzy Search by [G]rep' })
     vim.keymap.set('n', '<Leader>fb', builtin.buffers, { desc = '[F]uzzy Search existing [B]uffers' })
     -- :Telescope help_tags
