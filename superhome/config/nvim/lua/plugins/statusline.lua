dbaa139a788c4bc9c78475a56e00a006bb8deaae
Nodar Gogoberidze <gnodar01@gmail.com>
2025-03-21 13:44:37 -0400

    Add statusline

---
+++ superhome/config/nvim/lua/plugins/statusline.lua
@@ -0,0 +1,15 @@
+-- https://github.com/bluz71/nvim-linefly
+-- statusline
+
+return {
+  'bluz71/nvim-linefly',
+  name = 'nvim-linefly',
+  dependencies = {
+    'web-devicons',
+    'dap-ui',
+    'nvim-lint',
+    --'gitsigns',
+    --'possession',
+    --'nvim-possession',
+  },
+}
