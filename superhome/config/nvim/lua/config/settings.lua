fc43da0601d5b2b6f2886e3a75e9d30d53a2fe5f
Nodar Gogoberidze <gnodar01@gmail.com>
2025-03-25 15:11:43 -0400

    Clear path on terminal injection

--- superhome/config/nvim/lua/config/settings.lua
+++ superhome/config/nvim/lua/config/settings.lua
@@ -25,6 +25,7 @@ vim.api.nvim_create_autocmd('TermOpen', {
     if saved_path then
       vim.api.nvim_chan_send(vim.b.terminal_job_id, 'PATH=' .. saved_path .. '\n')
     end
+    saved_path = nil
   end,
 })
 
