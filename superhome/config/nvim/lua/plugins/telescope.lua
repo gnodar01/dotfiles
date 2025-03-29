ac20ebd44471192714f28a36dfcd83d64184450e
Nodar Gogoberidze <gnodar01@gmail.com>
2025-03-29 13:43:42 -0400

    Telescope toggle hidden

--- superhome/config/nvim/lua/plugins/telescope.lua
+++ superhome/config/nvim/lua/plugins/telescope.lua
@@ -61,12 +61,41 @@ return {
 
     -- See `:help telescope.builtin`
     local builtin = require('telescope.builtin')
+
+    -- toggle hidden with <C-h>
+    -- TODO: ignore .git, .pixi
+    -- https://github.com/nvim-telescope/telescope.nvim/issues/2874#issuecomment-1900967890
+    local my_find_files
+    my_find_files = function(opts, no_ignore)
+      opts = opts or {}
+      no_ignore = vim.F.if_nil(no_ignore, false)
+      opts.attach_mappings = function(_, map)
+        map({ 'n', 'i' }, '<C-h>', function(prompt_bufnr) -- <C-h> to toggle modes
+          local prompt = require('telescope.actions.state').get_current_line()
+          require('telescope.actions').close(prompt_bufnr)
+          no_ignore = not no_ignore
+          my_find_files({ default_text = prompt }, no_ignore)
+        end, { desc = 'Toggle Hidden' })
+        return true
+      end
+
+      if no_ignore then
+        opts.no_ignore = true
+        opts.hidden = true
+        opts.prompt_title = 'Find Files <ALL>'
+        builtin.find_files(opts)
+      else
+        opts.prompt_title = 'Find Files'
+        builtin.find_files(opts)
+      end
+    end
+
     vim.keymap.set('n', '<Leader>ff', function()
       local cwd = vim.fn.getcwd()
       if cwd == vim.fn.getenv('HOME') .. '/Developer/CellProfiler' then
-        builtin.find_files({ cwd = cwd .. '/CellProfiler/src' })
+        my_find_files({ cwd = cwd .. '/CellProfiler/src' })
       else
-        builtin.find_files()
+        my_find_files()
       end
     end, { desc = '[F]uzzy Search [F]iles' })
     vim.keymap.set('n', '<Leader>fj', builtin.git_files, { desc = '[F]uzzy Search [J]Git Files' })
