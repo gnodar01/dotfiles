09e46900711b581e9de7f52f7808728d4d5bcdc7
Nodar Gogoberidze <gnodar01@gmail.com>
2025-04-01 14:42:29 -0400

    Forgot indent-line add

---
+++ superhome/config/nvim/lua/plugins/indent-line.lua
@@ -0,0 +1,13 @@
+-- https://github.com/lukas-reineke/indent-blankline.nvim
+-- add indentation guides even on blank lines
+
+return {
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    name = 'indent-blankline',
+    -- see `:help ibl`
+    main = 'ibl',
+    -- see `:help ibl.config`
+    opts = {},
+  },
+}
