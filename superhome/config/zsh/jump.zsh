09c853b9ab7a15355ffad5d21246e50561b4cefc
Nodar Gogoberidze <gnodar01@gmail.com>
2025-10-11 17:51:55 -0400

    Credit authors

--- superhome/config/zsh/jump.zsh
+++ superhome/config/zsh/jump.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
+# https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/jump/jump.plugin.zsh
 
 # Easily jump around the file system by manually adding marks
 # marks are stored as symbolic links in the directory $MARKPATH (default $HOME/.marks)
-# https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/jump/jump.plugin.zsh
 #
 # jump FOO: jump to a mark named FOO
 # mark FOO: create a mark named FOO
