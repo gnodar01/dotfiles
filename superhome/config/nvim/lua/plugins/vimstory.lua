a9c1c6c3376f7adfd21353fa8c1a1b7497653b92
Nodar Gogoberidze <gnodar01@gmail.com>
2026-02-21 14:36:55 -0500

    [nvim] vimstory dev init

---
+++ superhome/config/nvim/lua/plugins/vimstory.lua
@@ -0,0 +1,9 @@
+return {
+  -- 'gnodar01/vimstory',
+  dir = '/Users/Nodar/Developer/vimstory',
+  name = 'vimstory',
+  --branch = 'dev',
+  dependencies = {},
+  -- config = function() end,
+  -- opts = {},
+}
