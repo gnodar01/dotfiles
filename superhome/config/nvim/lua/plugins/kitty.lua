c12967223dc9626c28b262d96aede93b5df9492c
Nodar Gogoberidze <gnodar01@gmail.com>
2026-06-22 16:46:13 -0400

    [nvim][kitty] Add kitty-scrollback.nvim

--- superhome/config/nvim/lua/plugins/kitty.lua
+++ superhome/config/nvim/lua/plugins/kitty.lua
@@ -5,4 +5,21 @@ return {
     ft = 'kitty',
     -- tag = "v1.5*",
   },
+  -- https://github.com/mikesmithgh/kitty-scrollback.nvim
+  {
+    'mikesmithgh/kitty-scrollback.nvim',
+    enabled = true,
+    lazy = true,
+    cmd = {
+      'KittyScrollbackGenerateKittens',
+      'KittyScrollbackCheckHealth',
+      'KittyScrollbackGenerateCommandLineEditing',
+    },
+    event = { 'User KittyScrollbackLaunch' },
+    -- version = '*', -- latest stable version, may have breaking changes if major version changed
+    -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
+    config = function()
+      require('kitty-scrollback').setup()
+    end,
+  },
 }
