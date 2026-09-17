c86ffbc3a5078570562a2d1caacde42e3f48faea
Nodar Gogoberidze <gnodar01@gmail.com>
2026-09-16 21:46:25 -0400

    [hyprland] Zoom

--- .config/yadm/alt/.config/hypr##hostname.magwai/hyprland.lua
+++ .config/yadm/alt/.config/hypr##hostname.magwai/hyprland.lua
@@ -397,3 +397,27 @@ hl.window_rule({
   move = '20 monitor_h-120',
   float = true,
 })
+
+-- Nodar EXTRAS UNTIL THIS IS MOVED AND ORGANIZED
+
+-- https://wiki.hypr.land/configuring/code-snippets/#windows-magnifier-like-cursor-zoom
+local MAX_ZOOM = 3
+local MIN_ZOOM = 1
+local ZOOM_TOGGLE_FACTOR = 1.5
+
+---@param offset number
+---@return nil
+local function zoom(offset)
+  local current = hl.get_config('cursor.zoom_factor')
+  if offset ~= nil then
+    current = current + offset
+  elseif current ~= MIN_ZOOM then
+    current = MIN_ZOOM
+  else
+    current = ZOOM_TOGGLE_FACTOR
+  end
+  current = math.max(MIN_ZOOM, math.min(MAX_ZOOM, current))
+  hl.config({ cursor = { zoom_factor = current } })
+end
+
+hl.bind('SUPER + ALT + 8', zoom)
