23c1771c14296ee59937d84c7c053671fa0fbb67
Nodar Gogoberidze <gnodar01@gmail.com>
2026-09-20 21:17:12 -0400

    [magwai][hyprland] Remove unnecessary inputs

--- .config/yadm/alt/.config/hypr##hostname.magwai/io/inputs.lua
+++ .config/yadm/alt/.config/hypr##hostname.magwai/io/inputs.lua
@@ -13,22 +13,5 @@ hl.config({
     follow_mouse = 1,
 
     sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
-
-    touchpad = {
-      natural_scroll = false,
-    },
   },
-})
-
-hl.gesture({
-  fingers = 3,
-  direction = 'horizontal',
-  action = 'workspace',
-})
-
--- Example per-device config
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
-hl.device({
-  name = 'epic-mouse-v1',
-  sensitivity = -0.5,
 })
