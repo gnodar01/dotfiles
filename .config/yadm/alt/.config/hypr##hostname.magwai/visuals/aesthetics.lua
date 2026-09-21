76666d4ba81d0abd451f5bac51f01dee76468ea9
Nodar Gogoberidze <gnodar01@gmail.com>
2026-09-20 20:51:58 -0400

    [magwai][hyprland] Small adjustments, reorg, cleanup

--- .config/yadm/alt/.config/hypr##hostname.magwai/visuals/aesthetics.lua
+++ .config/yadm/alt/.config/hypr##hostname.magwai/visuals/aesthetics.lua
@@ -2,26 +2,20 @@
 ---- LOOK AND FEEL ----
 -----------------------
 
--- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
   general = {
     gaps_in = 5,
-    gaps_out = 20,
+    gaps_out = 10,
 
     border_size = 2,
 
     col = {
-      active_border = { colors = { 'rgba(33ccffee)', 'rgba(00ff99ee)' }, angle = 45 },
-      inactive_border = 'rgba(595959aa)',
+      active_border = { colors = { '#8BE9FDEE', '#BD93F9EE' }, angle = 45 },
+      inactive_border = '#282A36AA',
     },
 
-    -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
-    resize_on_border = false,
-
-    -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
+    -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
     allow_tearing = false,
-
-    layout = 'dwindle',
   },
 
   decoration = {
@@ -31,91 +25,32 @@ hl.config({
     -- Change transparency of focused and unfocused windows
     active_opacity = 1.0,
     inactive_opacity = 1.0,
+    --active_opacity = 0.95,
+    --inactive_opacity = 0.85,
+
+    dim_modal = true,
 
     shadow = {
-      enabled = true,
+      enabled = false,
       range = 4,
       render_power = 3,
-      color = 0xee1a1a1a,
+      color = '#EE1A1A1A',
     },
 
     blur = {
-      enabled = true,
-      size = 3,
-      passes = 1,
+      enabled = false,
+      size = 2,
+      passes = 3,
       vibrancy = 0.1696,
+      popups = false,
+      noise = 0.2,
+      xray = false,
     },
-  },
 
-  animations = {
-    enabled = true,
-  },
-})
-
--- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
-hl.curve('easeOutQuint', { type = 'bezier', points = { { 0.23, 1 }, { 0.32, 1 } } })
-hl.curve('easeInOutCubic', { type = 'bezier', points = { { 0.65, 0.05 }, { 0.36, 1 } } })
-hl.curve('linear', { type = 'bezier', points = { { 0, 0 }, { 1, 1 } } })
-hl.curve('almostLinear', { type = 'bezier', points = { { 0.5, 0.5 }, { 0.75, 1 } } })
-hl.curve('quick', { type = 'bezier', points = { { 0.15, 0 }, { 0.1, 1 } } })
-
--- Default springs
-hl.curve('easy', { type = 'spring', mass = 1, stiffness = 238.1191, dampening = 24.21279333 })
-
-hl.animation({ leaf = 'global', enabled = true, speed = 10, bezier = 'default' })
-hl.animation({ leaf = 'border', enabled = true, speed = 5.39, bezier = 'easeOutQuint' })
-hl.animation({ leaf = 'windows', enabled = true, speed = 4.79, spring = 'easy' })
-hl.animation({ leaf = 'windowsIn', enabled = true, speed = 4.1, spring = 'easy', style = 'popin 87%' })
-hl.animation({ leaf = 'windowsOut', enabled = true, speed = 1.49, bezier = 'linear', style = 'popin 87%' })
-hl.animation({ leaf = 'fadeIn', enabled = true, speed = 1.73, bezier = 'almostLinear' })
-hl.animation({ leaf = 'fadeOut', enabled = true, speed = 1.46, bezier = 'almostLinear' })
-hl.animation({ leaf = 'fade', enabled = true, speed = 3.03, bezier = 'quick' })
-hl.animation({ leaf = 'layers', enabled = true, speed = 3.81, bezier = 'easeOutQuint' })
-hl.animation({ leaf = 'layersIn', enabled = true, speed = 4, bezier = 'easeOutQuint', style = 'fade' })
-hl.animation({ leaf = 'layersOut', enabled = true, speed = 1.5, bezier = 'linear', style = 'fade' })
-hl.animation({ leaf = 'fadeLayersIn', enabled = true, speed = 1.79, bezier = 'almostLinear' })
-hl.animation({ leaf = 'fadeLayersOut', enabled = true, speed = 1.39, bezier = 'almostLinear' })
-hl.animation({ leaf = 'workspaces', enabled = true, speed = 1.94, bezier = 'almostLinear', style = 'fade' })
-hl.animation({ leaf = 'workspacesIn', enabled = true, speed = 1.21, bezier = 'almostLinear', style = 'fade' })
-hl.animation({ leaf = 'workspacesOut', enabled = true, speed = 1.94, bezier = 'almostLinear', style = 'fade' })
-hl.animation({ leaf = 'zoomFactor', enabled = true, speed = 7, bezier = 'quick' })
-
--- Ref https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
--- "Smart gaps" / "No gaps when only"
--- uncomment all if you wish to use that.
--- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
--- hl.workspace_rule({ workspace = "f[1]",   gaps_out = 0, gaps_in = 0 })
--- hl.window_rule({
---     name  = "no-gaps-wtv1",
---     match = { float = false, workspace = "w[tv1]" },
---     border_size = 0,
---     rounding    = 0,
--- })
--- hl.window_rule({
---     name  = "no-gaps-f1",
---     match = { float = false, workspace = "f[1]" },
---     border_size = 0,
---     rounding    = 0,
--- })
-
--- See https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/ for more
-hl.config({
-  dwindle = {
-    preserve_split = true, -- You probably want this
-  },
-})
-
--- See https://wiki.hypr.land/Configuring/Layouts/Master-Layout/ for more
-hl.config({
-  master = {
-    new_status = 'master',
-  },
-})
-
--- See https://wiki.hypr.land/Configuring/Layouts/Scrolling-Layout/ for more
-hl.config({
-  scrolling = {
-    fullscreen_on_one_column = true,
+    glow = {
+      enabled = false,
+      range = 10,
+    },
   },
 })
 
--- .config/yadm/alt/.config/hypr##hostname.magwai/visuals/layouts.lua
+++ .config/yadm/alt/.config/hypr##hostname.magwai/visuals/layouts.lua
@@ -1,3 +1,58 @@
+-----------------------
+----    LAYOUTS    ----
+-----------------------
+
+hl.config({
+  general = {
+    -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
+    resize_on_border = false,
+    -- Resize on bottom right only; 1-4, 1 is top left, clockwise, 0 is off
+    --resize_corner = 3,
+
+    -- See https://wiki.hypr.land/Configuring/Layouts/
+    layout = 'scrolling',
+  },
+})
+
+-- Ref https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+-- "Smart gaps" / "No gaps when only"
+-- uncomment all if you wish to use that.
+-- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+-- hl.workspace_rule({ workspace = "f[1]",   gaps_out = 0, gaps_in = 0 })
+-- hl.window_rule({
+--     name  = "no-gaps-wtv1",
+--     match = { float = false, workspace = "w[tv1]" },
+--     border_size = 0,
+--     rounding    = 0,
+-- })
+-- hl.window_rule({
+--     name  = "no-gaps-f1",
+--     match = { float = false, workspace = "f[1]" },
+--     border_size = 0,
+--     rounding    = 0,
+-- })
+
+-- See https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/ for more
+hl.config({
+  dwindle = {
+    preserve_split = true, -- You probably want this
+  },
+})
+
+-- See https://wiki.hypr.land/Configuring/Layouts/Master-Layout/ for more
+hl.config({
+  master = {
+    new_status = 'master',
+  },
+})
+
+-- See https://wiki.hypr.land/Configuring/Layouts/Scrolling-Layout/ for more
+hl.config({
+  scrolling = {
+    fullscreen_on_one_column = true,
+  },
+})
+
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
 --------------------------------
